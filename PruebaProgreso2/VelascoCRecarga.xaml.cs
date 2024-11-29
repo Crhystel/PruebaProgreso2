15f4510a55b858d8230c1fed2515b6009b9e2858
Crhystel <crhystelvelasco213@gmail.com>
2024-11-29 07:35:14 -0500

    Labels y entry para pagina de recarga medio listos

---
+++ PruebaProgreso2/VelascoCRecarga.xaml.cs
@@ -0,0 +1,14 @@
+namespace PruebaProgreso2;
+
+public partial class VelascoCRecarga : ContentPage
+{
+	public VelascoCRecarga()
+	{
+		InitializeComponent();
+	}
+
+    private void Buton1_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
