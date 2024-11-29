4beb59814d28c519cb5432e9905d5825f539ebb7
Crhystel <crhystelvelasco213@gmail.com>
2024-11-29 07:07:32 -0500

    Agregar archivos de proyecto.

---
+++ PruebaProgreso2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebaProgreso2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PruebaProgreso2/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PruebaProgreso2
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
